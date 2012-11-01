38bbdeeaff5d47542b1bccfce6110908b2e0217c
YuAo <yuao@eicodesign.com>
2012-11-01 18:34:59 +0800

    import all necessary files in MessagePack.h

--- MessagePack.h
+++ MessagePack.h
@@ -1,3 +1,11 @@
+#ifndef MessagePack_MessagePack_h
+#define MessagePack_MessagePack_h
+
 #import "NSData+MessagePack.h"
 #import "NSDictionary+MessagePack.h"
 #import "NSArray+MessagePack.h"
+
+#import "MessagePackPacker.h"
+#import "MessagePackParser.h"
+
+#endif
