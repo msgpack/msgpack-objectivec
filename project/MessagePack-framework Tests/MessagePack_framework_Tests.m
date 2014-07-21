27b73b46c4f4eafa3c5a4cb7d5946a8846ab02bd
Charles Francoise <charles.francoise@gmail.com>
2014-07-21 23:17:15 +0200

    add test target

---
+++ project/MessagePack-framework Tests/MessagePack_framework_Tests.m
@@ -0,0 +1,34 @@
+//
+//  MessagePack_framework_Tests.m
+//  MessagePack-framework Tests
+//
+//  Created by Charles Francoise on 21/07/14.
+//  Copyright (c) 2014 Charles Francoise. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MessagePack_framework_Tests : XCTestCase
+
+@end
+
+@implementation MessagePack_framework_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testPackArray
+{
+    
+}
+
+@end
